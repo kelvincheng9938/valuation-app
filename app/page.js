d864aca5cdd0d6c61fa4d348c67e817a00a5a950
kelvincheng9938 <kelvincheng9938@gmail.com>
2025-08-29 11:18:42 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,22 +1,5 @@
-'use client'
-import { useState } from 'react'
 import HomePage from '@/components/HomePage'
-import ReportPage from '@/components/ReportPage'
-import NewsPage from '@/components/NewsPage'
-import Navigation from '@/components/Navigation'
-import './globals.css'
 
-export default function App() {
-  const [currentPage, setCurrentPage] = useState('home')
-
-  return (
-    <div className="min-h-screen">
-      <Navigation currentPage={currentPage} setPage={setCurrentPage} />
-      <main>
-        {currentPage === 'home' && <HomePage setPage={setCurrentPage} />}
-        {currentPage === 'report' && <ReportPage />}
-        {currentPage === 'news' && <NewsPage />}
-      </main>
-    </div>
-  )
+export default function Home() {
+  return <HomePage />
 }
