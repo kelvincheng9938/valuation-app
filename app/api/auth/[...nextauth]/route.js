c237ba5cfe5a11163b2895b2a58914d2c10965dc
kelvincheng9938 <kelvincheng9938@gmail.com>
2025-09-04 16:44:56 +0800

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,2 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-
-const handler = NextAuth({
-  providers: [
-    Google({
-              clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-
-    })
-  ],
-  session: { strategy: "jwt" },
-  secret: process.env.NEXTAUTH_SECRET
-});
-
-export { handler as GET, handler as POST };
+// app/api/auth/[...nextauth]/route.js
+export const { GET, POST } = (await import('@/auth')).handlers;
